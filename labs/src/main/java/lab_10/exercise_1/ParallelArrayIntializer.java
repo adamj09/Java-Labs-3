4dfc6e78fb81a8d88e35511d1bf619e23ba8e266
AdamJ <adamjohnston144@gmail.com>
2025-11-27 12:33:22 -0500

    finished lab 10 exercise 1

--- labs/src/main/java/lab_10/exercise_1/ParallelArrayIntializer.java
+++ labs/src/main/java/lab_10/exercise_1/ParallelArrayIntializer.java
@@ -1,34 +1,65 @@
 package lab_10.exercise_1;
 
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 public class ParallelArrayIntializer {
     public static void main(String[] args) {
-        double[] list1 = new double[9000000];
-        double[] list2 = new double[9000000];
-        
+        final int SIZE = 9000000;
+        double[] list1 = new double[SIZE];
+        double[] list2 = new double[SIZE];
+
         long startTime = System.nanoTime();
         assignValues(list1);
         long endTime = System.nanoTime();
         long elapsedTime = endTime - startTime;
-        System.out.println("Sequential array initialization elapsed time (ms): " + (double)elapsedTime / (1000000));
+        System.out.println("Sequential time [ms]: " + (double) elapsedTime / (1000000));
 
         startTime = System.nanoTime();
         parallelAssignValues(list2);
         endTime = System.nanoTime();
         elapsedTime = endTime - startTime;
 
-        System.out.println("Parallel array initialization elapsed time (ms): " + (double)elapsedTime / (1000000));
+        System.out.println("Parallel time [ms]: " + (double) elapsedTime / (1000000));
     }
 
     public static void parallelAssignValues(double[] list) {
-        //TODO: implement parallel value assignment
+        RecursiveAction mainTask = new AssignTask(list, 0, list.length);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(mainTask);
     }
 
     public static void assignValues(double[] list) {
         Random random = new Random();
-        for(int i = 0; i < list.length; i++) {
-            list[i] = random.nextDouble() * 100; // Assign a random value between 0 and 100 (inclusive).
+        for (int i = 0; i < list.length; i++) {
+            list[i] = random.nextDouble();
         }
+    }
+
+    private static class AssignTask extends RecursiveAction {
+        private final int THRESHOLD = 500;
+        private int high, low;
+        private double[] list;
+
+        AssignTask(double[] list, int low, int high) {
+            this.list = list;
+            this.high = high;
+            this.low = low;
+        }
+
+        @Override
+        protected void compute() {
+            Random random = new Random();
+            if (high - low < THRESHOLD) {
+                for(int i = low; i < high; i++) {
+                    list[i] = random.nextDouble();
+                }
+            } else {
+                int mid = (low + high) / 2;
+                invokeAll(new AssignTask(list, low, mid), new AssignTask(list, mid, high));
+            }
+        }
+
     }
 }
